9a24ed929d916875f99406b34fd7cd004afb9a39
solfonte <msfontenla@fi.uba.ar>
2022-11-10 16:00:20 -0300

    feat: added send push notification logic

--- notifications_service/controllers/notification_controller.py
+++ notifications_service/controllers/notification_controller.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi import APIRouter, status, HTTPException, Depends, Request
 from sqlalchemy.orm import Session
 from typing import List, Union
 from notifications_service.database import schema, database, notification_repository
+
 #from users_service.utils import authorization_handler, token_handler, firebase_handler
 
 notification_router = APIRouter()
@@ -10,3 +11,11 @@ notification_repository = Notification_repository()
 @notification_router.post("", status_code=status.HTTP_201_CREATED)
 async def register_device(device: schema.Device, db: Session = Depends(database.get_db)):
         return notification_repository.register_device(device,db)
+
+
+@notification_router.post("/{id/]", status_code=status.HTTP_201_CREATED):
+async def notify_driver(user_id: int, req: Request, db: Session = Depends(database.get_db)):
+        result = notification_repository.send_push_notification(json.loads(req.body),user_id, db)
+        #TODO: que pasa si no hay devices? devuelve error o como lo manejamos?
+        #tambien que pasa si no hay un usuario registrado.
+        return result
--- notifications_service/database/notification_repository.py
+++ notifications_service/database/notification_repository.py
@@ -2,13 +2,27 @@ from . import models, schema
 from sqlalchemy.orm import Session
 
 class Notification_repository():
-    def register_device(device: schema.Device, db: Session):
+    def register_device(self, device: schema.Device, db: Session):
         
-        deb_device = models.Device(
+        db_device = models.Device(
             user_id=device.user_id
             device_id=device.device_id
         )
-        db.add(deb_device)
+        db.add(db_device)
         db.commit()
         #db.refresh(deb_device)
-        return deb_device
+        return db_device
+    
+    def get_device_tokens_from_user(self, userId: int, db: Session):
+        #pasar a un crud
+        return db.query(models.Device).filter(models.Device.user_id == userId)
+
+    def send_push_notification(self,notification_parameters: Dict, userId: int, db: Session):
+        tokens = self.get_device_tokens_from_user(user_id, db)
+        #TODO: investigar que devuelve el batch response
+        batch_response = fmc.sendPush(
+                device_tokens=tokens,
+                data=notification_parameters.get('data'),
+                title=notification_parameters.get('title'),
+                body=notification_parameters.get('body')
+            )
